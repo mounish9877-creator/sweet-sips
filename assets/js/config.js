f70ed236620bede8649677a414bf548d760b0d2e
mounish9877-creator <mounish9877@gmail.com>
2025-08-27 16:12:32 -0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,3 @@
 // Paste your Google Apps Script 'Web App' URL below
 // Example: https://script.google.com/macros/s/XXXX/exec
-const SWEET_SIPS_API = "https://script.google.com/macros/s/AKfycby3Ek6LZkOsf2krQnMBDxl-_W2P_DaVn1e6BTPxDS6pzM0ja9Ga2tTvlH6JlVeOEUIPgA/exec";
+const SWEET_SIPS_API = "";
